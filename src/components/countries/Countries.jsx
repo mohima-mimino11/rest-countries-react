5bda6ba84f4754fd893643146bcdae859e395860
mohima-mimino11 <mohimahoque22@gmail.com>
2025-01-24 13:24:32 +0600

    state changing of useState in array with spread operator and event handling function parameter handled with wrapper arrow function added

--- src/components/countries/Countries.jsx
+++ src/components/countries/Countries.jsx
@@ -5,17 +5,41 @@ import './Countries.css'
 
 const Countries = () => {
   const [countries, setCountries] = useState([]);
+  const [visitedCountries, setVisitedCountries] = useState([]);
+
   useEffect(() => {
     fetch('https://restcountries.com/v3.1/all')
       .then(res => res.json())
       .then(data => setCountries(data))
   },[])
+
+  const handleVisitedCountry = (country) =>{
+    // console.log('add this to your visited country');
+    // console.log(country);
+    // need to use spread operator to copy older array and add new array elements as React's DOM doesn't recognize array methods like push, pop as it works under useState hook of react of state changing and setting state
+    const newVisitedCountries = [...visitedCountries, country]
+    setVisitedCountries(newVisitedCountries)
+    
+
+  }
   return (
     <div>
       <h3>Countries: {countries.length}</h3>
+      <div>
+        <h5>Visited Countries: {visitedCountries.length} </h5>
+        <ul>
+          {
+            // a unique key is needed so that the propType error doesn't occur
+            visitedCountries.map(country => <li key={country?.cca3}>{country?.name?.common}</li>)
+          }
+        </ul>
+      </div>
       <div className="country-container">
       {
-        countries.map(country => <Country key={country.cca3} country={country}></Country>)
+        countries.map(country => <Country 
+          key={country.cca3} 
+          handleVisitedCountry = {handleVisitedCountry}
+          country={country}></Country>)
       }
       </div>
     </div>
--- src/components/country/Country.jsx
+++ src/components/country/Country.jsx
@@ -1,12 +1,16 @@
 import { useState } from 'react';
 import './Country.css'
-const Country = ({country}) => {
-  console.log(country);
+const Country = ({country, handleVisitedCountry}) => {
+  // console.log(country);
   const {name, flags, population, area, cca3} = country;
   const [visited, SetVisited] = useState(false);
   const handleVisited = () =>{
     SetVisited(!visited)
   }
+  // console.log(handleVisitedCountry);
+  
+
+  
   return (
     <div className={`country ${visited ? 'visited' : 'non-visited'} `}>
       <h3 style={{color: visited ? 'black' : 'white'}}>Name:{name?.common} </h3>
@@ -14,6 +18,9 @@ const Country = ({country}) => {
       <p>Population: {population}</p>
       <p>Area: {area}</p>
       <p><small>Code: {cca3}</small></p>
+      {/* as react handles function call on click events as a default call and do the functionality without clicking the functions that needs parameter need to be handled in a wrapper anonymous arrow function. */}
+      <button onClick={() => handleVisitedCountry(country)}>Mark Visited</button>
+      <br />
       <button onClick={handleVisited}>{visited ? 'Visited' : 'Going'}</button>
       {visited ? 'I have visited this country' : 'I want to visit this country'}
     </div>
